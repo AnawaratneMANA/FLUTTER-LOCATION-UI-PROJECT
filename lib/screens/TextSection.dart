725e3272851735f543ae15f06a5cbe0c72d5fe7a
AnawaratneMANA <it19086408@my.sliit.lk>
2022-02-22 19:32:34 +0530

    final value updated _color

--- lib/screens/TextSection.dart
+++ lib/screens/TextSection.dart
@@ -2,12 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TextSection extends StatelessWidget {
-  Color? _color;
+  final Color _color;
 
   // Constructor (Either optional parameters, Named parameters)
-  TextSection( Color color) {
-    this._color = color;
-  }
+  TextSection( this._color); // Automatically assigned to the attributes.
 
   @override
   Widget build(BuildContext context){
