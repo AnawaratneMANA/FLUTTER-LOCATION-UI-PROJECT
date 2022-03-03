b823fbbc65ef6ee77fb04464c87069f7e1c41b9f
AnawaratneMANA <it19086408@my.sliit.lk>
2022-03-03 13:59:09 +0530

    External Style file added.

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lab2/screens/location.dart';
+import 'style.dart';
 
 class App extends StatelessWidget {
 
@@ -9,6 +10,12 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LocationDetails(),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          toolbarTextStyle: TextTheme(titleMedium: AppBarTextStyle ).bodyText2,
+            titleTextStyle: TextTheme(titleMedium: AppBarTextStyle ).headline6
+        )
+      )
     );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,15 @@
+// Common Style file.
+
+import 'package:flutter/material.dart';
+const LargeTextSize = 26.0;
+const MediumTextSize = 20.0;
+const BodyTextSize =  16.0;
+
+const String FontNameDefault = "Raleway";
+
+const AppBarTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w300,
+  fontSize: MediumTextSize,
+  color: Colors.white,
+);
